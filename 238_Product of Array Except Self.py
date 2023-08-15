5f4d22a9f60535395a01d52c89ba0b22c704697c
chaeonee <31956177+chaeonee@users.noreply.github.com>
2023-08-15 12:57:45 +0900

    Update 238_Product of Array Except Self.py

--- 238_Product of Array Except Self.py
+++ 238_Product of Array Except Self.py
@@ -1,5 +1,21 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        N = len(nums)
+
+        answer = [1] * N
+        for i in range(N-1):
+            answer[i+1] = nums[i] * answer[i]
+
+        num = 1
+        for i in range(N-1, -1, -1):
+            answer[i] *= num
+            num *= nums[i]
+
+        return answer
+        
+'''
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
         p_num, n_zero = 1, 0
         for n in nums:
             if n == 0:
@@ -21,3 +37,4 @@ class Solution:
                     result.append(0)
 
         return result
+'''
